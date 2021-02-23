96458d014329fd50859826928363393536b62fa3
Shahadat Hossain Shaki <spider.shaki@gmail.com>
2021-02-24 03:43:43 +0600

    Update Facebook Signin.java

--- Facebook Signin.java
+++ Facebook Signin.java
@@ -16,11 +16,14 @@
 
 
 private void generatePublishedKeyHashForFacebook() {
-        //From the play console app signing we get a sha1 and from the Sha-1 we can generate the key hash
-        byte[] sha1 = {
-                (byte) 0x93, 0x1E, 0x7A, 0x24, (byte) 0xA4, (byte) 0xAD, 0x1D, 0x37, 0x4E, 0x49, 0x5E, (byte) 0xD9, 0x78, (byte) 0xF1, 0x18, 0x59, (byte) 0x8A, 0x0E, (byte) 0xD9, (byte) 0xA9
-        };
-        System.out.println("keyhashGooglePlaySignIn:"+ Base64.encodeToString(sha1, Base64.NO_WRAP));
+        String string = "41:0A:FF:5D:4D:F0:39:52:AA:3A:E2:FF:4A:FF:04:4E:85:67:D2:1B";
+        String[] v = string.split(":");
+        byte[] sha1 = new byte[v.length];
+        int i = 0;
+        for(String val: v) {
+            sha1[i++] =  Integer.decode("0x" + val).byteValue();
+        }
+        Log.d("keyhash:",  Base64.encodeToString(sha1, Base64.NO_WRAP));
     }
 
 
